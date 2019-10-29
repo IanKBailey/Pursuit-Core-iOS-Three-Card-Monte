7c95200bde46e12738e1ecbd4b12d3d2ab86e2c9
Ian Bailey <iankbailey2@gmail.com>
2019-10-29 17:23:16 -0400

    basic game works, Need to reveal winning card if user picks a losing card

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-
-
-
-
-
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var card0: UIButton!
+    @IBOutlet weak var card1: UIButton!
+    @IBOutlet weak var card2: UIButton!
+    @IBOutlet weak var gameStateLabel: UILabel!
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-
+    @IBAction func flipCard(_ sender: UIButton) {
+        let range = 0...2
+        let randomRange = range.randomElement()
+        
+        if sender.tag == randomRange {
+            sender.setImage(UIImage.init(named: "kingCard"), for: .normal)
+            gameStateLabel.text = "You win"
+            disable()
+        } else  {
+            sender.setImage(UIImage.init(named: "threeCard"), for:  .normal)
+            gameStateLabel.text = "You Lose"
+            disable()
+        }
+        
+    }
+    func disable() {
+        card0.isEnabled = false
+        card1.isEnabled = false
+        card2.isEnabled = false
+    }
+    
 }
 
