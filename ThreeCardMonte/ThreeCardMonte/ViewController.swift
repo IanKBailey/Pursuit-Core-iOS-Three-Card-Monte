61639cf3ba47343b9f5dc9391f12f5242d5aaf34
Ian Bailey <iankbailey2@gmail.com>
2019-10-30 15:44:42 -0400

    refactoring

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -16,24 +16,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var card2: UIButton!
     @IBOutlet weak var gameStateLabel: UILabel!
     
+    var buttons: [UIButton] = []
+    
+    let cardImages = [UIImage(named: "threeCard")!,UIImage(named: "threeCard")!, UIImage(named: "kingCard")!]
     
     
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    buttons = [card0, card1, card2]
   }
 
     @IBAction func flipCard(_ sender: UIButton) {
-        let range = 0...2
-        let randomRange = range.randomElement()
+        let randomImage = cardImages.randomElement() ?? UIImage(named: "threeCard")!
+                
+        var tags = Array(0...2) // 2
+        tags.append(sender.tag)
         
-        if sender.tag == randomRange {
-            sender.setImage(UIImage.init(named: "kingCard"), for: .normal)
+        if randomImage == UIImage(named: "kingCard")! {
+            sender.setImage(UIImage(named: "kingCard"), for: .normal)
             gameStateLabel.text = "You win"
             disable()
-        } else  {
-            sender.setImage(UIImage.init(named: "threeCard"), for:  .normal)
+        } else {
+            sender.setImage(UIImage(named: "threeCard"), for:  .normal)
             gameStateLabel.text = "You Lose"
+            for tag in tags {
+                if sender.tag != tag {
+                    let button = buttons[tag]
+                    button.setImage(UIImage(named: "kingCard"), for: .normal)
+                    break
+                }
+            }
+            
             disable()
         }
         
@@ -43,6 +57,22 @@ class ViewController: UIViewController {
         card1.isEnabled = false
         card2.isEnabled = false
     }
+    
+    
+    @IBAction func reset(_ sender: UIButton) {
+        let buttonArray = [card0, card1, card2]
+        for button in buttonArray {
+            button?.setImage(UIImage(named: "cardBackRed"), for: .normal)
+            button?.isEnabled = true
+        }
+    
+    }
+    
+    
+    
+    
+    
+    
     
 }
 
